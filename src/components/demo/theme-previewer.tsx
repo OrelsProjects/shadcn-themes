9bbd8ddb0dc6057d368a2635163e89e42a20926f
orelsprojects <orelsmail@gmail.com>
2024-12-14 16:05:43 +0200

    Before massive changes

--- src/components/demo/theme-previewer.tsx
+++ src/components/demo/theme-previewer.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { Button as ButtonDemo } from "@/components/ui-demo/button";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui-demo/input";
 import { Label } from "@/components/ui-demo/label";
 import {
@@ -30,24 +29,47 @@ import {
 import { useAppDispatch, useAppSelector } from "@/lib/hooks/redux";
 import { cn, getThemeColor } from "@/lib/utils";
 import { selectPalette } from "@/lib/features/theme/paletteSlice";
+import { ParsedPalette } from "@/models/palette";
+import {
+  Dialog,
+  DialogHeader,
+  DialogTrigger,
+  DialogContent,
+} from "@/components/ui-demo/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui-demo/dropdown-menu";
 
 export default function ThemePreviewer() {
-  const {
-    selectedPalette,
-    selectedPaletteName,
-    selectedThemeType,
-    allPalettes,
-  } = useAppSelector(state => state.palette);
+  const { selectedPaletteName, selectedThemeType, allPalettes } =
+    useAppSelector(state => state.palette);
 
   const dispatch = useAppDispatch();
+
+  const ownerToPalettes = useMemo(() => {
+    const ownerToPalettesMap = new Map<string, ParsedPalette[]>();
+    allPalettes.forEach(palette => {
+      const owner = palette.owner;
+      if (ownerToPalettesMap.has(owner)) {
+        ownerToPalettesMap.get(owner)?.push(palette);
+      } else {
+        ownerToPalettesMap.set(owner, [palette]);
+      }
+    });
+    console.log(ownerToPalettesMap);
+    return ownerToPalettesMap;
+  }, [allPalettes]);
 
   return (
     <div className="min-h-screen flex flex-col">
       {/* shadcn Components Section (3/5 of the screen) */}
-      <div className="flex-grow max-h-[60vh] p-6 overflow-y-auto bg-background-demo text-foreground-demo">
+      <div className="flex-grow max-h-[50vh] p-6 overflow-y-auto bg-background-demo text-foreground-demo">
         <h1 className="text-3xl font-bold mb-6">shadcn Components Preview</h1>
 
-        <div className="space-y-8">
+        <div className="space-y-8 grid  grid-cols-1 md:grid-cols-3 gap-2 ">
           <CardDemo>
             <CardHeaderDemo>
               <CardTitleDemo>Account Settings</CardTitleDemo>
@@ -66,11 +88,22 @@ export default function ThemePreviewer() {
               </div>
             </CardContentDemo>
             <CardFooterDemo>
-              <ButtonDemo>Save Changes</ButtonDemo>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <ButtonDemo>Save Changes</ButtonDemo>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <p className="text-lg font-bold">Are you sure?</p>
+                  </DialogHeader>
+                  <ButtonDemo>Yes</ButtonDemo>
+                  <ButtonDemo variant="outline">No</ButtonDemo>
+                </DialogContent>
+              </Dialog>
             </CardFooterDemo>
           </CardDemo>
 
-          <Tabs defaultValue="account" className="w-full">
+          <Tabs defaultValue="account" className="w-full h-full">
             <TabsList>
               <TabsTrigger value="account">Account</TabsTrigger>
               <TabsTrigger value="password">Password</TabsTrigger>
@@ -85,10 +118,16 @@ export default function ThemePreviewer() {
                   </CardDescriptionDemo>
                 </CardHeaderDemo>
                 <CardContentDemo className="space-y-2">
-                  <div className="space-y-1">
-                    <Label htmlFor="username">Username</Label>
-                    <Input id="username" defaultValue="@shadcn" />
-                  </div>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <ButtonDemo variant={"outline"}>Accounts</ButtonDemo>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      <DropdownMenuItem> User </DropdownMenuItem>
+                      <DropdownMenuItem> Admin </DropdownMenuItem>
+                      <DropdownMenuItem> Superadmin </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </CardContentDemo>
                 <CardFooterDemo>
                   <ButtonDemo>Save changes</ButtonDemo>
@@ -124,83 +163,91 @@ export default function ThemePreviewer() {
       </div>
 
       {/* Theme Preview Section (2/5 of the screen) */}
-      <div className="fixed bottom-0 w-full h-[40vh] bg-background p-6 overflow-x-auto">
-        <div className="grid grid-cols-8 gap-10 space-x-4">
-          {Object.entries(allPalettes).map(([paletteName, palette]) => (
-            <Card
-              key={paletteName}
-              onClick={() => dispatch(selectPalette({ name: paletteName }))}
-              className={cn("w-40 flex-shrink-0 hover:cursor-pointer", {
-                "border-primary": paletteName === selectedPaletteName,
-              })}
-            >
-              <CardHeader>
-                <CardTitle>
-                  {paletteName
-                    .split("-")
-                    .map(x => x[0].toUpperCase() + x.slice(1))
-                    .join(" ")}
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <div className="grid grid-cols-2 gap-2">
-                  <div className="space-y-2">
-                    <div
-                      className="w-full h-8 rounded"
-                      style={{
-                        backgroundColor: getThemeColor(
-                          "primary",
-                          palette[selectedThemeType],
-                          true,
-                        ),
-                      }}
-                    />
-                    <p className="text-xs">Primary</p>
-                  </div>
-                  <div className="space-y-2">
-                    <div
-                      className="w-full h-8 rounded"
-                      style={{
-                        backgroundColor: getThemeColor(
-                          "secondary",
-                          palette[selectedThemeType],
-                          true,
-                        ),
-                      }}
-                    />
-                    <p className="text-xs">Secondary</p>
-                  </div>
-                  <div className="space-y-2">
-                    <div
-                      className="w-full h-8 rounded"
-                      style={{
-                        backgroundColor: getThemeColor(
-                          "background",
-                          palette[selectedThemeType],
-                          true,
-                        ),
-                      }}
-                    />
-                    <p className="text-xs">Background</p>
-                  </div>
-                  <div className="space-y-2">
-                    <div
-                      className="w-full h-8 rounded"
-                      style={{
-                        backgroundColor: getThemeColor(
-                          "card",
-                          palette[selectedThemeType],
-                          true,
-                        ),
-                      }}
-                    />
-                    <p className="text-xs">Card</p>
-                  </div>
-                </div>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
+      <div className="fixed bottom-0 w-full h-[50vh] bg-background p-6 overflow-x-auto space-y-10 border-t-2 border-primary">
+        {/* Make sections by palette.owner */}
+        {Array.from(ownerToPalettes.keys()).map(owner => (
+          <div key={owner} className="flex flex-col space-y-4">
+            <h2 className="text-2xl text-white font-bold">{owner}</h2>
+            <div className="grid grid-cols-8 gap-10 space-x-4">
+              {ownerToPalettes.get(owner)!.map(palette => (
+                <Card
+                  key={palette.name}
+                  onClick={() =>
+                    dispatch(selectPalette({ name: palette.name }))
+                  }
+                  className={cn("w-40 flex-shrink-0 hover:cursor-pointer", {
+                    "border-primary": palette.name === selectedPaletteName,
+                  })}
+                >
+                  <CardHeader>
+                    <CardTitle>
+                      {palette.name
+                        .split("-")
+                        .map(x => x[0].toUpperCase() + x.slice(1))
+                        .join(" ")}
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="grid grid-cols-2 gap-2">
+                      <div className="space-y-2">
+                        <div
+                          className="w-full h-8 rounded"
+                          style={{
+                            backgroundColor: getThemeColor(
+                              "primary",
+                              palette.colors[selectedThemeType],
+                              true,
+                            ),
+                          }}
+                        />
+                        <p className="text-xs">Primary</p>
+                      </div>
+                      <div className="space-y-2">
+                        <div
+                          className="w-full h-8 rounded"
+                          style={{
+                            backgroundColor: getThemeColor(
+                              "secondary",
+                              palette.colors[selectedThemeType],
+                              true,
+                            ),
+                          }}
+                        />
+                        <p className="text-xs">Secondary</p>
+                      </div>
+                      <div className="space-y-2">
+                        <div
+                          className="w-full h-8 rounded"
+                          style={{
+                            backgroundColor: getThemeColor(
+                              "background",
+                              palette.colors[selectedThemeType],
+                              true,
+                            ),
+                          }}
+                        />
+                        <p className="text-xs">Background</p>
+                      </div>
+                      <div className="space-y-2">
+                        <div
+                          className="w-full h-8 rounded"
+                          style={{
+                            backgroundColor: getThemeColor(
+                              "card",
+                              palette.colors[selectedThemeType],
+                              true,
+                            ),
+                          }}
+                        />
+                        <p className="text-xs">Card</p>
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
 
       {/* Theme Toggle */}
