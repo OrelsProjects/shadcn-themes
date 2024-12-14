b697981707c3b261e2e0349b8f1b42b81043c172
orelsprojects <orelsmail@gmail.com>
2024-12-14 22:28:11 +0200

    Another try

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true, // Ensures strict mode is enabled
+  swcMinify: true, // Enables SWC-based minification (faster)
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 export default function Dashboard() {
   return (
     <main className="min-h-screen bg-background text-foreground relative flex flex-col space-y-8">
-      {/* <Header />
+      <Header />
       <div className="w-full h-full flex flex-col gap-12 pt-16 pb-32">
         <div className="container h-48 flex flex-col justify-start items-center gap-10">
           <h1 className="text-6xl font-semibold flex justify-center">
@@ -34,7 +34,7 @@ export default function Dashboard() {
           <ThemePreviewer />
         </div>
       </div>
-      <BottomNavbar /> */}
+      <BottomNavbar />
     </main>
   );
 }
