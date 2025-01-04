d81187b4922dd14b989efaeded81e84b5cc72ccc
orelsprojects <orelsmail@gmail.com>
2025-01-04 21:06:52 +0200

    Fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,35 +6,34 @@ import type { NextRequest } from "next/server";
 const MAX_TIME_TO_PROCESS_REQUEST = 30;
 
 export function middleware(req: NextRequest) {
-  return NextResponse.next();
-  // let requestTimestamp =
-  //   req.headers.get("X-Request-Timestamp") ||
-  //   req.headers.get("x-request-timestamp");
-  // if (!requestTimestamp) {
-  //   console.log("Header entries: ", req.headers.entries());
-  //   return NextResponse.json(
-  //     { error: "Invalid request, no timestamp" },
-  //     { status: 400 },
-  //   );
-  // }
-  // requestTimestamp = decrypt(requestTimestamp);
-  // const url = req.nextUrl.clone();
-  // let isValidRequest = false;
+  let requestTimestamp =
+    req.headers.get("X-Request-Timestamp") ||
+    req.headers.get("x-request-timestamp");
+  if (!requestTimestamp) {
+    console.log("Header entries: ", req.headers.entries());
+    return NextResponse.json(
+      { error: "Invalid request, no timestamp" },
+      { status: 400 },
+    );
+  }
+  requestTimestamp = decrypt(requestTimestamp);
+  const url = req.nextUrl.clone();
+  let isValidRequest = false;
 
-  // if (requestTimestamp) {
-  //   const requestTime = parseInt(requestTimestamp, 10);
-  //   const currentTime = Date.now();
-  //   const diffInSec = (currentTime - requestTime) / 1000;
+  if (requestTimestamp) {
+    const requestTime = parseInt(requestTimestamp, 10);
+    const currentTime = Date.now();
+    const diffInSec = (currentTime - requestTime) / 1000;
 
-  //   if (diffInSec <= MAX_TIME_TO_PROCESS_REQUEST) {
-  //     isValidRequest = true;
-  //   }
-  // }
-  // if (!isValidRequest) {
-  //   return NextResponse.json({ error: "Invalid request" }, { status: 400 });
-  // }
+    if (diffInSec <= MAX_TIME_TO_PROCESS_REQUEST) {
+      isValidRequest = true;
+    }
+  }
+  if (!isValidRequest) {
+    return NextResponse.json({ error: "Invalid request" }, { status: 400 });
+  }
 
-  // return NextResponse.rewrite(url);
+  return NextResponse.rewrite(url);
 }
 
 export const config = {
